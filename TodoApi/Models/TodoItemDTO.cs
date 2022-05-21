bbc8723eb30156eb86da549be59e66f30510fec5
Wenjiang Xu <scac59414@gmail.com>
2022-05-21 21:14:49 +0800

    first commit

---
+++ TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// This subset of a model is usually referred to as a
+/// Data Transfer Object (DTO), input model, or view model.
+/// Used to:
+/// Prevent over-posting.
+/// Hide properties that clients are not supposed to view.
+/// Omit some properties in order to reduce payload size.
+/// Flatten object graphs that contain nested objects. Flattened object graphs can be more convenient for clients.
+/// </summary>
+
+namespace TodoApi.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
