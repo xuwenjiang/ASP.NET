a76c60a48b90ec8e4da2b30d2f393cfe02bd6567
Wenjiang Xu <wenjiangxu@microsoft.com>
2022-05-22 23:03:04 +0800

    some configs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -11,19 +11,26 @@ builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoL
 builder.Services.AddEndpointsApiExplorer();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "AzurePlay1",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseDeveloperExceptionPage();
 
     // Swagger.
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
